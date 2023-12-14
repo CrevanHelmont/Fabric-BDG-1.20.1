6cf6160601ecd1cb4efc5f5786e49946773a9123
CrevanHelmont <ArcCrevanHelmont@gmail.com>
2023-12-14 04:49:32 -0600

    Initial commit

---
+++ src/main/java/net/helmont/beedengumi/BeeDengumiClient.java
@@ -0,0 +1,12 @@
+package net.helmont.beedengumi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeeDengumiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
